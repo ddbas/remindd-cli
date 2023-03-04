a9f23becdc48febdb2c37f88a164c84d541ff214
David Bastien <velours.ingrate_00@icloud.com>
2023-03-04 18:55:10 -0500

    feat: enter raw mode and clear terminal

--- src/commands/list/interactive.ts
+++ src/commands/list/interactive.ts
@@ -1,5 +1,35 @@
+import { stdin, stdout } from 'node:process';
+import * as readline from 'node:readline';
+
 const interactive = () => {
-    console.warn('Not implemented.');
+    if (!stdin.isTTY) {
+        throw new Error('Must be run in a terminal.');
+    }
+
+    readline.emitKeypressEvents(stdin);
+    stdin.setRawMode(true);
+
+    stdout.cursorTo(0, 0);
+    stdout.clearScreenDown();
+
+    stdin.on(
+        'keypress',
+        (
+            str: string,
+            key: {
+                name: string;
+                ctrl: boolean;
+                meta: boolean;
+                shift: boolean;
+            }
+        ) => {
+            if (key.ctrl && key.name == 'c') {
+                stdin.setRawMode(false);
+                process.exit();
+                return;
+            }
+        }
+    );
 };
 
 export default interactive;
